fdde63c15af1c6f3ed352b38e29dbbcb758e38dd
Muhammad <vipmsiriev@mail.ru>
2023-12-06 04:12:50 +0300

    Полностью сделан Home(Самая главная страничка)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import styles from "../Header/Header.module.css";
-import logo from '../../assets/Logo.svg'
+import logo from "../../assets/Logo.svg";
 // import logotip from "../../assets/log4.png";
 import { motion } from "framer-motion";
 import { forwardRef, useRef } from "react";
@@ -9,25 +9,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { exit } from "../../features/applicationSlice";
 
 const Header = forwardRef(() => {
-  const dispatch = useDispatch()
-  const token = useSelector(state => state.application.token)
-  const user = useSelector(state => state.application.user)
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.application.token);
+  const user = useSelector((state) => state.application.user);
   const handleClose = () => {
-    dispatch(exit())
-  }  
+    dispatch(exit());
+  };
   const textAnimation = {
     hidden: (custom) => ({
-      x: 100,
+      scaleX: 0,
       opacity: 0,
-      transition: { easy: custom * 3 },
+      transition: { delay: custom * 2 },
     }),
     visible: (custom) => ({
-      x: 0,
+      scaleX: 1,
       opacity: 1,
     }),
   };
 
-  if(token) {
+  if (token) {
     return (
       <>
         <motion.div
@@ -56,22 +56,24 @@ const Header = forwardRef(() => {
               </ul>
             </div>
             <Link to={"/"} className={styles.logoPlace}>
-              <img
-                src={logo}
-                alt=""
-                width={130}
-                height={60}
-              />
+              <img src={logo} alt="" width={130} height={60} />
             </Link>
-  
+
             <div className={styles.rightMenu}>
               <ul>
                 <li>
                   <Link to={"/signup"}>{user.login}</Link>
-                  <img src={`http://localhost:3030/${user.avatar}`} alt=""  width={50} height={50}/>
+                  <img
+                    src={`http://localhost:3030/${user.avatar}`}
+                    alt=""
+                    width={50}
+                    height={50}
+                  />
                 </li>
                 <li>
-                  <Link to={"/"} onClick={handleClose}>Выйти</Link>
+                  <Link to={"/"} onClick={handleClose}>
+                    Выйти
+                  </Link>
                 </li>
                 <li>
                   <Link to={"/"}>Помощь</Link>
@@ -125,12 +127,7 @@ const Header = forwardRef(() => {
             </ul>
           </div>
           <Link to={"/"} className={styles.logoPlace}>
-            <img
-              src={logo}
-              alt=""
-              width={130}
-              height={60}
-            />
+            <img src={logo} alt="" width={130} height={60} />
           </Link>
 
           <div className={styles.rightMenu}>
